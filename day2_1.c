1c8f6dd1944135139d3890c1467d6818fe285d6c
Shreyabansal31 <121783671+Shreyabansal31@users.noreply.github.com>
2023-01-09 16:17:46 +0530

    Create day2_1.c

---
+++ day2_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+  int i, j, n, k;
+  printf("Enter the no. of lines in pattern\n");
+  scanf("%d", &n);
+  for (i = 1; i <= n; i++) {
+    k = i;
+    for (j = 1; j <= i; j++) {
+      printf("%d", k);
+      k++;
+    }
+    printf("\n");
+  }
+  return 0;
+}
+Footer
+© 2023 GitHub, Inc.
